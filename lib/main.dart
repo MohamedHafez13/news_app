b3a9b10f096cdc993eb3bf22c7ac431aacf84057
MohamedHafez13 <muhammedhaffezz@gmail.com>
2021-11-04 01:14:36 +0200

    permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/home_states.dart';
-import 'package:news_app/layout/layout.dart';
 import 'package:news_app/layout/layout_screen.dart';
-import 'package:news_app/modules/business_screen.dart';
 import 'package:news_app/shared/network/dio__helper.dart';
 
 import 'layout/home_Cubit.dart';
@@ -28,6 +26,11 @@ class MyApp extends StatelessWidget {
         listener: (context, state) {},
         builder: (context, state) {
           return MaterialApp(
+            darkTheme:ThemeData(
+              brightness: Brightness.dark,
+              backgroundColor: Colors.black
+            ) ,
+            themeMode: ThemeMode.dark,
             theme: ThemeData(
               accentColor: Colors.teal,
               scaffoldBackgroundColor: Colors.white,
